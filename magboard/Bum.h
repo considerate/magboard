65a06968396d42e31a66e3239392956b2b1d63ce
Joss Clifford-Frith <jossie.cliffordfrith@gmail.com>
2012-09-21 02:17:35 +0900

    Phew, that was close.

---
+++ magboard/Bum.h
@@ -0,0 +1,13 @@
+//
+//  Bum.h
+//  magboard
+//
+//  Created by Jocelyn Clifford-Frith on 21/09/2012.
+//  Copyright (c) 2012 Gripenskolan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Bum : NSObject
+
+@end
---
+++ magboard/Bum.m
@@ -0,0 +1,13 @@
+//
+//  Bum.m
+//  magboard
+//
+//  Created by Jocelyn Clifford-Frith on 21/09/2012.
+//  Copyright (c) 2012 Gripenskolan. All rights reserved.
+//
+
+#import "Bum.h"
+
+@implementation Bum
+
+@end
