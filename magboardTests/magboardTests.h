e99ba2d5efabef4ffae569b03f2608bf0646d80d
Viktor Kronvall <viktor.kronvall@gmail.com>
2012-09-21 01:57:46 +0900

    Initial Commit

---
+++ magboardTests/magboardTests.h
@@ -0,0 +1,13 @@
+//
+//  magboardTests.h
+//  magboardTests
+//
+//  Created by Viktor Kronvall on 2012/09/21.
+//  Copyright (c) 2012年 Gripenskolan. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface magboardTests : SenTestCase
+
+@end
---
+++ magboardTests/magboardTests.m
@@ -0,0 +1,32 @@
+//
+//  magboardTests.m
+//  magboardTests
+//
+//  Created by Viktor Kronvall on 2012/09/21.
+//  Copyright (c) 2012年 Gripenskolan. All rights reserved.
+//
+
+#import "magboardTests.h"
+
+@implementation magboardTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in magboardTests");
+}
+
+@end
